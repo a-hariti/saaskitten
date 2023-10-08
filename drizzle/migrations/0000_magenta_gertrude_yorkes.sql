19b9e90f98fcbf8b2df7701b8f2d5d284d7d95f1
Abdellah Hariti <haritiabdellah@gmail.com>
2023-10-08 11:40:01 +0100

    collapse migrations

--- drizzle/migrations/0000_magenta_gertrude_yorkes.sql
+++ drizzle/migrations/0000_magenta_gertrude_yorkes.sql
@@ -1,3 +1,9 @@
+DO $$ BEGIN
+ CREATE TYPE "plans" AS ENUM('free', 'pro');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_keys" (
 	"id" varchar(255) PRIMARY KEY NOT NULL,
 	"user_id" varchar(15) NOT NULL,
@@ -8,6 +14,11 @@ CREATE TABLE IF NOT EXISTS "password_reset_token" (
 	"id" varchar(128) PRIMARY KEY NOT NULL,
 	"user_id" varchar(15) NOT NULL,
 	"expires" bigint NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user_plans" (
+	"user_id" varchar(15) PRIMARY KEY NOT NULL,
+	"plan" "plans" DEFAULT 'free' NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_sessions" (
@@ -31,25 +42,31 @@ CREATE TABLE IF NOT EXISTS "email_verification_token" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_keys" ADD CONSTRAINT "user_keys_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "user_keys" ADD CONSTRAINT "user_keys_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "password_reset_token" ADD CONSTRAINT "password_reset_token_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "password_reset_token" ADD CONSTRAINT "password_reset_token_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "user_plans" ADD CONSTRAINT "user_plans_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "email_verification_token" ADD CONSTRAINT "email_verification_token_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "email_verification_token" ADD CONSTRAINT "email_verification_token_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- drizzle/migrations/0003_melodic_wind_dancer.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE "user_credits";--> statement-breakpoint
-ALTER TABLE "user_plans" ADD PRIMARY KEY ("user_id");
